876a915d966cbee8045064a4347705ee726eaba7
kissy2 <51262744+kissy2@users.noreply.github.com>
2022-01-31 04:28:43 +0100

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -8,9 +8,6 @@ require 'PHPMailer-6.5/src/Exception.php';
 require 'PHPMailer-6.5/src/PHPMailer.php';
 require 'PHPMailer-6.5/src/SMTP.php';
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 // include 'vendor/autoload.php';
 include 'web.config.php';
