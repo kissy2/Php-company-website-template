aaf4c7b24426f0f8283fed5dd928d0e076791c58
kissy2 <illfight4uu@gmail.com>
2022-01-31 04:20:46 +0100

    new version phpmailer

---
+++ forms/web.config.php
@@ -0,0 +1,15 @@
+<?
+<?php
+
+$config = [
+
+'SENDER_EMAIL' => 'email',
+
+'SENDER_EMAIL_PASSWORD' => 'pass',
+
+'NAME_SENDER_EMAIL' =>'Rama Solutions Email Forwarder (website)',
+
+'DESTINATION_EMAIL' => 'email',
+
+];
+?>
